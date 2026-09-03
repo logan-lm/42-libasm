505da6631e843c6fc17d7569ee4d32f40156c6a8
Logan <contact@loganmartin.fr>
2026-09-03 15:13:31 +0200

    add(mandatory part): better tests

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
 #include <errno.h>
 #include <string.h>
 
@@ -15,32 +16,72 @@ int main(void)
 	char a[] = "hello";
 	char b[] = "byebye";
 
-	printf("%lu\n", ft_strlen(a));
+	printf("a: %s\n", a);
+	printf("b: %s\n", b);
 
-	printf("%d\n", ft_strcmp(a, b));
+
+	printf("\n-----ft_strlen-----\n");
+	printf("a: %lu\n", ft_strlen(a));
+	printf("\"\": %lu\n", ft_strlen(""));
+	// SHOULD SEGFAULT
+	// printf("%lu\n", ft_strlen(NULL));
+
+
+	printf("\n-----ft_strcmp-----\n");
+	printf("a, b: %d\n", ft_strcmp(a, b));
+	printf("\"a\", \"a\": %d\n", ft_strcmp("a", "a"));
+	printf("\"a\", \"c\": %d\n", ft_strcmp("a", "c"));
+	printf("\"c\", \"a\": %d\n", ft_strcmp("c", "a"));
+	printf("\"aa\", \"a\": %d\n", ft_strcmp("aa", "a"));
+	//SHOULD SEGFAULT
+	// printf("%d\n", ft_strcmp("c", NULL));
 
 	char *cpy = ft_strdup(b);
 
-	printf("%s\n", ft_strcpy(b, a));
+	printf("\n-----ft_strcpy-----\n");
+	printf("a -> b\n");
+	printf("ret: %s\n", ft_strcpy(b, a));
+	printf("a: %s\n", a);
+	printf("b: %s\n", b);
+	// SHOULD SEGFAULT
+	// printf("ret: %s\n", ft_strcpy(b, NULL));
 
+
+	printf("\n-----ft_write------\n");
 	int ret = ft_write(1, "coucou\n", 7);
-	printf("Write returned %d\n", ret);
-	if (ret < 0)
-		printf("errno has been set to : %s\n", strerror(errno));
+	printf("return: %d\n", ret);
+	ret = ft_write(3, "coucou\n", 7);
+	printf("fd 3 return: %d\n", ret);
+	printf("errno has been set to : %s\n", strerror(errno));
+	ret = ft_write(1, NULL, 7);
+	printf("NULL ptr return: %d\n", ret);
+	printf("errno has been set to : %s\n", strerror(errno));
+	ret = ft_write(1, "coucou\n", -1);
+	printf("negative size return: %d\n", ret);
+	printf("errno has been set to : %s\n", strerror(errno));
+	ret = ft_write(1, "coucou\n", -0);
+	printf("0 size return: %d\n", ret);
 
+
+	printf("\n------ft_read------\n");
 	char str[100] = {0};
+	printf("write a test value : ");
+	fflush(stdout);
 	ret = ft_read(1, str, 99);
+	printf("\n");
 	printf("Read returned %d\n", ret);
 	if (ret < 0)
 		printf("errno has been set to : %s\n", strerror(errno));
 	else
 	{
 		str[ret] = '\0';
-		printf("Read : %s\n", str);
+		printf("your value was : %s\n", str);
 	}
 
+	printf("\n-----ft_strdup-----\n");
 	if (!cpy)
 		printf("ft_strup failed to allocate memory\n");
 	else
-		printf("duplicated string : %s\n", cpy);
+		printf("duplicated b before ft_strcpy: %s\n", cpy);
+	free(cpy);
 }
