d397ea1ba9939b692d4a448c3b0b6a8ac1a42f95
Logan <contact@loganmartin.fr>
2026-09-03 15:15:54 +0200

    add(bonus): list_size

---
+++ ft_list_size.s
@@ -0,0 +1,14 @@
+global ft_list_size
+
+section .text
+
+ft_list_size:
+	xor     rax, rax
+.loop:
+	test    rdi, rdi
+	jz      .end
+	mov     rdi, [rdi + 8]
+	inc     rax
+	jmp     .loop
+.end:
+	ret
